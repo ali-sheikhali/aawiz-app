9e9b73872c4df40bdb12f0dcab7e8860a408acdf
ali-sheikhali <ali.sheikhali1998@gmail.com>
2025-11-27 11:54:03 +0330

    feat: implement login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,22 +4,31 @@ import FormWrapper from "@/components/common/FormWrapper";
 import { loginSchema, LoginSchemaType } from "@/schemas/loginSchema";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Button from "@/components/base/Button";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 export default function LoginPage() {
   const {
     register,
     handleSubmit,
-    formState : { errors },
+    formState: { errors },
   } = useForm<LoginSchemaType>({
     resolver: zodResolver(loginSchema),
   });
-
+  const router = useRouter();
   const onsubmit = (data: LoginSchemaType) => {
-    console.log(data);
+    console.log("login", data);
+    if (data.email === "aawiz@gmail.com" && data.password === "123456") {
+      toast.success("login");
+      router.push("/");
+    } else {
+      toast.error("not login");
+    }
   };
 
   return (
     <main
-      className={`w-full min-h-screen flex flex-col justify-center items-center bg-gray-500 `}
+      className={`w-full min-h-screen flex flex-col justify-center items-center bg-gray-200 `}
     >
       <div className="w-10/12 max-w-sm bg-white p-6 rounded-xl shadow-lg flex flex-col gap-6">
         <h1 className="text-center font-bold text-lg">wellcome to aawiz app</h1>
@@ -29,8 +38,22 @@ export default function LoginPage() {
             register={{ ...register("email") }}
             errors={errors?.email}
             isRequire={true}
+            placeholder="ali@gmail.com"
           />
+          <FormInput
+            label="password"
+            register={{ ...register("password") }}
+            errors={errors?.password}
+            isRequire={true}
+            placeholder="123456"
+          />
+          <Button type="submit" name="login" />
         </FormWrapper>
+        <div className="text-xs text-stroke-secondary">
+          for login
+          <p>email: aawiz@gmail.com</p>
+          <p>password: 123456</p>
+        </div>
       </div>
     </main>
   );
---
+++ components/base/Button.tsx
@@ -0,0 +1,19 @@
+interface ButtonProps {
+  name: string;
+  type: "submit" | "button";
+  variant?: "primary" | "secondary";
+}
+
+export default function Button({ name, type = "submit" , variant="primary" }: ButtonProps) {
+
+  const base = "w-full text-center py-2 rounded-md"  
+   const variants = {
+    primary: "bg-green text-fill-primary hover:bg-blue-700",
+    secondary: "bg-gray-200 dark:bg-gray-700 dark:text-white"
+  };  
+  return (
+    <div>
+      <button className={`${base} ${variants[variant]}`} type={type}>{name}</button>
+    </div>
+  );
+}
